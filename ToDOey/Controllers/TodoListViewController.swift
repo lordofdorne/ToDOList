945c5448ebcc82751bb061085f8db5629121f823
asim samuel <asim.samuel1@gmail.com>
2019-04-04 18:19:12 -0400

    Persisting data using Core Data

--- ToDOey/Controllers/TodoListViewController.swift
+++ ToDOey/Controllers/TodoListViewController.swift
@@ -7,42 +7,31 @@
 //
 
 import UIKit
+import CoreData
 
-class TodoListViewController: UITableViewController {
+class TodoListViewController: UITableViewController  {
     
     var itemArray = [Item]()
+   
+    var selectedCategory : Category? {
+        didSet{
+            loadItems()
+        }
+    }
     
-    let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Items.plist")
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    
+    
     
     let defaults = UserDefaults.standard
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        print(FileManager.default.urls(for: .documentDirectory, in: .userDomainMask))
         
-        print(dataFilePath)
-        loadItems()
-        
-        // Do any additional setup after loading the view, typically from a nib.
-      let newItem = Item()
-        newItem.title =  "Practice Code"
-        itemArray.append(newItem)
-        
-        let newItem2 = Item()
-        newItem2.title =  "Practice Code"
-        itemArray.append(newItem2)
-        
-        let newItem3 = Item()
-        newItem3.title =  "Practice Code"
-        itemArray.append(newItem3)
+      
         
         
-        
-        
-
-//
-//        if let items = defaults.array(forKey: "TodoListArray") as? [Item] {
-//            itemArray = items
-//        }
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -61,32 +50,21 @@ class TodoListViewController: UITableViewController {
         cell.accessoryType = item.done  ? .checkmark : .none
         
       
-        
-        
         return cell
     }
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        // print(itemArray[indexPath.row])
-     
-        itemArray[indexPath.row].done = !itemArray[indexPath.row].done
+//        context.delete(itemArray[indexPath.row])
+//        itemArray.remove(at: indexPath.row)
+//
 
+        
+     itemArray[indexPath.row].done = !itemArray[indexPath.row].done
 
+        saveItems()
         
-        
-        
-        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
-            tableView.cellForRow(at: indexPath)?.accessoryType = .none
-        } else {
-            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
-        }
-        
-        tableView.reloadData()
         tableView.deselectRow(at: indexPath, animated: true)
     }
-    
-    // Add New Items
-    
     
     @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
         
@@ -97,12 +75,15 @@ class TodoListViewController: UITableViewController {
         let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
             //what will happen once user clicks the Add button on UIAlert
             
-           let newItem = Item()
-            newItem.title = textField.text!
-           
-            self.itemArray.append(newItem)
+          
             
-            self.saveItems()
+            let newItem = Item(context: self.context)
+            newItem.title = textField.text!
+            newItem.done = false
+            newItem.parentCategory = self.selectedCategory
+           self.itemArray.append(newItem)
+            
+        self.saveItems()
            
            
         }
@@ -117,30 +98,67 @@ class TodoListViewController: UITableViewController {
     }
     
     func saveItems() {
-        let encoder = PropertyListEncoder()
+        
         
         do {
-            let data = try encoder.encode(self.itemArray)
-            try data.write(to: self.dataFilePath!)
+         try context.save()
         } catch {
-            print("error encoding item array, \(error)")
+            print("Error saving context \(error)")
             
         }
         self.tableView.reloadData()
         
     }
-    
-    func loadItems() {
+
+    func loadItems(with request: NSFetchRequest<Item> = Item.fetchRequest(), predicate : NSPredicate? = nil) {
         
-        if let data = try? Data(contentsOf: dataFilePath!) {
-            let decoder = PropertyListDecoder()
-            do {
-            itemArray = try decoder.decode( [Item].self, from: data)
-                } catch {
-                print("Error decoding item, \(error) ")
-                }
+        let categoryPredicate = NSPredicate(format: "parentCategory.header MATCHES %@", selectedCategory!.header!)
+
+        if let additionalPredicate = predicate {
+            request.predicate = NSCompoundPredicate(andPredicateWithSubpredicates: [categoryPredicate, additionalPredicate])
+        } else {
+            request.predicate = categoryPredicate
         }
+        
+
+        do {
+             itemArray = try context.fetch(request)
+    }    catch {
+             print("Error fetching Data \(error)")
     }
 
 }
+    
+    
 
+}
+
+//MARK: - Search Bar Methods
+extension TodoListViewController: UISearchBarDelegate {
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+    
+        let request : NSFetchRequest<Item> = Item.fetchRequest()
+        
+      let predicate = NSPredicate(format: "title CONTAINS[cd] %@", searchBar.text!)
+        
+        request.sortDescriptors = [NSSortDescriptor(key: "title", ascending: true)]
+        
+        loadItems(with: request, predicate: predicate)
+        
+        
+  
+  }
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        if searchBar.text?.count == 0 {
+            loadItems()
+            
+            DispatchQueue.main.async {
+                 searchBar.resignFirstResponder()
+            }
+            
+           
+        }
+    }
+}
---
+++ ToDOey/Data Model/CategoryItem.swift
@@ -0,0 +1,18 @@
+//
+//  CategoryItem.swift
+//  ToDOey
+//
+//  Created by Asim on 4/1/19.
+//  Copyright © 2019 Asim Samuel. All rights reserved.
+//
+
+import Foundation
+
+class CategoryItem {
+    
+    var header : String = ""
+    var finish : Bool = false
+    
+    
+    
+}
